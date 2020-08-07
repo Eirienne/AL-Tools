07720b89cea6506214e96cc7d487bdb9a5013076
Eirienne <benyamin.tandiono516@gmail.com>
2020-08-07 16:49:03 +0700

    creating template.html

---
+++ JavaScript/AirStrikeCalc.js
@@ -0,0 +1,20 @@
+
+function getPlaneCD() {
+    var x = document.getElementById("plane1").value;
+    document.getElementById("plane1cd").value = x;
+}
+
+function getPlane2CD() {
+    var x = document.getElementById("plane2").value;
+    document.getElementById("plane2cd").value = x;
+}
+
+function getPlane3CD() {
+    var x = document.getElementById("plane3").value;
+    document.getElementById("plane3cd").value = x;
+}
+
+function getCharRld() {
+    var x = document.getElementById("characters").value;
+    document.getElementById("character-rld").value = x;
+}
