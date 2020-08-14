ff87e8888c381f434224115a095db93388086ceb
Eirienne <benyamin.tandiono516@gmail.com>
2020-08-14 09:14:58 +0700

    New Navbar and Menu

--- JavaScript/AirStrikeCalc.js
+++ JavaScript/AirStrikeCalc.js
@@ -1,4 +1,3 @@
-
 function getPlaneCD() {
     var x = document.getElementById("plane1").value;
     document.getElementById("plane1cd").value = x;
@@ -17,4 +16,38 @@ function getPlane3CD() {
 function getCharRld() {
     var x = document.getElementById("characters").value;
     document.getElementById("character-rld").value = x;
+}
+
+function calculateRld() {
+
+    akagaValue = 0;
+    beaconValue = 0;
+
+    var planeCD1 = document.getElementById("plane1cd");
+    var planeCD2 = document.getElementById("plane2cd");
+    var planeCD3 = document.getElementById("plane3cd");
+
+    var akaga = document.getElementById("akaga");
+    var beacon = document.getElementById("beacon");
+
+    total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+
+    if (akaga.checked == true) {
+        akagaValue = akaga.value;
+        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+    }
+
+    if (beacon.checked == true) {
+        beaconValue = beacon.value;
+        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+    }
+
+    if (akaga.checked == true && beacon.checked == true) {
+        akagaValue = akaga.value;
+        beaconValue = beacon.value;
+        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+    }
+
+    document.getElementById("totalCD").value = total;
+
 }
