966cceab94b3bdcc3dda37ca6be9ea7c5eb13376
Eirienne <benyamin.tandiono516@gmail.com>
2020-08-18 13:24:34 +0700

    Change select option to Script

--- JavaScript/AirStrikeCalc.js
+++ JavaScript/AirStrikeCalc.js
@@ -30,24 +30,38 @@ function calculateRld() {
     var akaga = document.getElementById("akaga");
     var beacon = document.getElementById("beacon");
 
-    total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+    total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD3.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
 
     if (akaga.checked == true) {
         akagaValue = akaga.value;
-        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD3.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
     }
 
     if (beacon.checked == true) {
         beaconValue = beacon.value;
-        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD3.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
     }
 
     if (akaga.checked == true && beacon.checked == true) {
         akagaValue = akaga.value;
         beaconValue = beacon.value;
-        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD2.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
+        total = parseFloat(planeCD1.value) + parseFloat(planeCD2.value) + parseFloat(planeCD3.value) + parseFloat(akagaValue) + parseFloat(beaconValue);
     }
 
     document.getElementById("totalCD").value = total;
 
+    document.getElementById("totalCDCustom").value = total;
+
+}
+
+function reset() {
+    document.getElementById("character-rld").value = 0;
+    document.getElementById("custom-character-rld").value = 0;
+    document.getElementById("plane1cd").value = 0;
+    document.getElementById("plane2cd").value = 0;
+    document.getElementById("plane3cd").value = 0;
+
+    document.getElementById("totalCD").value = 0;
+
+    document.getElementById("totalCDCustom").value = 0;
 }
