b3c1b0e9c388beffae66a34c04083c1e8abc2420
Eirienne <benyamin.tandiono516@gmail.com>
2020-11-17 14:06:16 +0700

    Update AL char details 2

--- JavaScript/AL-CharDetail.js
+++ JavaScript/AL-CharDetail.js
@@ -7,21 +7,21 @@ $("#characters").change(function () {
         console.log(characters);
         switch (characters) {
             case "--Select--":
-                img.src = "/Images/Logo/None.png";
+                img.src = "Images/Logo/None.png";
                 document.getElementById("plane1number").value = 0;
                 document.getElementById("plane2number").value = 0;
                 document.getElementById("plane3number").value = 0;
                 break;
 
             case "Akagi":
-                img.src = "/Images/AzurLane/Icon/AkagiIcon.png";
+                img.src = "Images/AzurLane/Icon/AkagiIcon.png";
                 document.getElementById("plane1number").value = 3;
                 document.getElementById("plane2number").value = 3;
                 document.getElementById("plane3number").value = 2;
                 break;
 
             case "Akagi (Muse)":
-                img.src = "/Images/AzurLane/Icon/Akagi_µIcon.png";
+                img.src = "Images/AzurLane/Icon/Akagi_µIcon.png";
                 document.getElementById("plane1number").value = 3;
                 document.getElementById("plane2number").value = 3;
                 document.getElementById("plane3number").value = 2;
